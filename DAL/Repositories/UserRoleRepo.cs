2c00b144ef194b6419f91c923982880e834c6796
David De Vilder <redlife@hotmail.fr>
2022-10-19 10:22:28 +0200

    17.10.22 -> 16h47

--- DAL/Repositories/UserRoleRepo.cs
+++ DAL/Repositories/UserRoleRepo.cs
@@ -33,40 +33,23 @@ namespace DAL.Repositories
         /// <returns></returns>
         public bool addOwner(int id)
         {
-            Command cmd = new Command("insert into User_Role (idRole IdUser)values (3,@idUser);");
+            Command cmd = new Command("insert into User_Role (idRole,IdUser)values (3,@idUser);");
             cmd.AddParameter("idUser", id);
-            
+
 
             /*
             //cmd.CommandType = CommandType.StoredProcedure;
             cmd.CommandText ="insert into User_Role (idRole IdUser)values (3,@idUser);";
-
-                    cmd.Parameters.AddWithValue("idUser", id);
-
-                    cnx.Open();
-                    cmd.ExecuteNonQuery
-                    
-                    using (SqlDataReader reader = cmd.ExecuteReader())
-                    {
-                        return GetList<Role>(reader);
-                    }
-                    
-                
-            */
+                cmd.Parameters.AddWithValue("idUser", id);
+                cnx.Open();
+                cmd.ExecuteNonQuery
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    return GetList<Role>(reader);
+                }
+              */
             return false;
         }
-
-
-
-
-
-
-
-
-
-
-          
-        
         /// <summary>
         /// Retire le role owner a l user 
         /// </summary>
