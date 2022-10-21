27d9058ff923929b9176a211598665ba7946c4db
Mehdy Maessckal <mehdy.maessckal@gmail.com>
2022-10-21 10:23:52 +0200

    Add table Step_User

---
+++ CrowdFundingDB/Tables/Step_User.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Step_User]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	IdUser INT NOT NULL,
+	IdProject INT NOT NULL,
+	IdStep INT NOT NULL
+
+	FOREIGN KEY(IdUser) REFERENCES [User](Id),
+	FOREIGN KEY(IdProject) REFERENCES Project(Id),
+	FOREIGN KEY(IdStep) REFERENCES Step(id)
+)
